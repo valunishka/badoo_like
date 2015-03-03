b90f593b4b47802e91f89d03331bfac8aef89465
Valunishka <ndj.vfvfieifnfk@yandex.ru>
2015-03-04 00:31:28 +0100

    First implementation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # badoo_like
+
+1. Install Greasemonkey and paste this script
+2. Login to Badoo and go to search page
+3. Open js-console and run
+```
+badooLiker.initialize()
+```
+4. Go to next search results page and repeat step 3
+
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // ==UserScript==
 // @name         Badoo liker
-// @namespace    http://your.homepage/
 // @version      0.1
 // @description  sup
 // @author       Valunishka
@@ -8,32 +7,70 @@
 // @grant        none
 // ==/UserScript==
 
-var loadScript = function( src, callback ) {
-	var s,r,t;
-	r = false;
-	s = document.createElement('script');
-	s.type = 'text/javascript';
-	s.src = src;
-	s.onload = s.onreadystatechange = function() {
-		if ( !r && (!this.readyState || this.readyState === 'complete') ) {
-			r = true;
-			callback();
-		}
+window.badooLiker = (function() {
+
+	var xhrDataSettings = {};
+
+	var loadScript = function( src, callback ) {
+		var s,r,t;
+		r = false;
+		s = document.createElement('script');
+		s.type = 'text/javascript';
+		s.src = src;
+		s.onload = s.onreadystatechange = function() {
+			if ( !r && (!this.readyState || this.readyState === 'complete') ) {
+				r = true;
+				callback();
+			}
+		};
+		t = document.getElementsByTagName('script')[0];
+		t.parentNode.insertBefore( s, t );
 	};
-	t = document.getElementsByTagName('script')[0];
-	t.parentNode.insertBefore( s, t );
-};
 
-var initialize = function() {
+	var initialize = function() {
+		loadScript('https://code.jquery.com/jquery-2.1.3.min.js', function() {
+			xhrDataSettings[ 'ws' ] = 1;
+			xhrDataSettings[ 'rt' ] = $('.js-ovl-open').data('ovlUrl').match(/rt=([\w\d]+)&/)[1];
 
-};
+			makeMagic();
+		});
 
-var makeMagic = function() {
+	};
 
-};
+	var makeMagic = function() {
+		var linksToProfiles = $('.b-link.c_name').map(function( index, element) {
+			return element.href;
+		});
 
-loadScript('https://code.jquery.com/jquery-2.1.3.min.js', function() {
-	console.log("----- Script loaded ------");
-	initialize();
-	makeMagic();
-});
+		linksToProfiles.each(function( index, url ) {
+			loadProfilePage( url )
+				.done(likeThisUser);
+		});
+	};
+
+	var loadProfilePage = function( url ) {
+		return $.ajax({
+			type: 'GET',
+			url: url
+		});
+	};
+
+	var likeThisUser = function( userPage ) {
+		var euri = userPage.match(/data-choice="yes"\sdata-url="\/hidden\?euri=(.{192})/);
+
+		if ( !euri ) return;
+
+		$.ajax({
+			url: '/hidden',
+			data: {
+				euri: euri[1],
+				ws: xhrDataSettings[ 'ws' ],
+				ts: xhrDataSettings[ 'ts' ]
+			}
+		});
+	};
+
+	return {
+		initialize: initialize
+	};
+})();
